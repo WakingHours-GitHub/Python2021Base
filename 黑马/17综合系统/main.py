7d3c583195b5179e0e3f4bea02dfbfb11aed94c0
WakingHours-GitHub <WakingHoursHUC@outlook.com>
2022-09-11 21:39:37 +0800

    9.8

--- 黑马/17综合系统/main.py
+++ 黑马/17综合系统/main.py
@@ -6,7 +6,9 @@
 #  程序入口文件，就是运行这个文件, 从而启动我们的项目
 # 导入模块:
 from mangerSystem import *
+# 如果报红线, 那么我们可以将这个文件夹remark as source root
 
 if __name__ == '__main__':  # 只有当本模块启动时, __name__变量才是__main__, 此时才进入if语句.
     # run system
     StudentManager().run()  # run
+
