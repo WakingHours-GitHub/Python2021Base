1b4a5430c43695a8b3263e0b3712f484c79a2580
WakingHours-GitHub <WakingHoursHUC@outlook.com>
2022-09-12 16:12:55 +0800

    9.12

--- 黑马/17综合系统/mangerSystem.py
+++ 黑马/17综合系统/mangerSystem.py
@@ -23,13 +23,17 @@
 
 
 """
+from typing import List
+
+from student import *
 
 
 # 定义管理系统的类
 class StudentManager(object):
     def __init__(self):
         # definite object attribute
-        self.student_list = list()  # create list type.
+        self.student_list: List[Student] = list()  # create list type.
+        # 这里指定为list类型, 成员变量为Student对象.
 
     # 管理系统内部的框架:
     # 程序入口:
@@ -45,19 +49,17 @@ class StudentManager(object):
 
             # execute different function.
             if menu_num == 1:  # add
-                pass
+                self.add_student()
             elif menu_num == 2:  # delete
-                pass
+                self.delete_student()
             elif menu_num == 3:  # alter(modify)
-                pass
+                self.modify_student()
             elif menu_num == 4:  # search information
-                pass
+                self.inquire_student()
             elif menu_num == 5:  # show all information
-                pass
-
+                self.show_all_student()
             elif menu_num == 6:  # save student information
-                pass
-
+                self.save_student()
             elif menu_num == 7:  # exit system
                 print("exit student management system")
                 break
@@ -80,6 +82,7 @@ class StudentManager(object):
             8. load student information
         """
         print("""
+    --------------------------------
         system function menu:
             1. add student
             2. delete student
@@ -88,10 +91,80 @@ class StudentManager(object):
             5. show all student information
             6. save student information
             7. exit system
+    --------------------------------
         """)
 
     def add_student(self):
+        """用户输入学员姓名, 性别, 手机号, 然后添加到系统当中去. """
         print("add student")
+        while True:
+            input_infor = input("please input name, gender, tel, and split by space(enter 'q' to exit): \n")
+            if input_infor == 'q':  # only input 'q' to exit loop
+                break
+            name, gender, tel = tuple(input_infor.strip().split(' '))
+            s = Student(name, gender, tel)  # 创建对象
+            self.student_list.append(s)  # 添加到当前学生列表当中去.
+
+        # print(self.student_list) # 测试
+
+    def delete_student(self):
+        """input target name, to delete this student """
+        print("delete student")
+        while True:
+            name = input("please input name that delete name(input 'q' to exit): ")
+            if name == 'q':
+                break
+            # elif name in [name_list.name for name_list in self.student_list]: # 直接使用列表生成式.
+            #     print(f"already deleted \'{name}\' student")
+            #     self.student_list.remove()
+            # else:
+            #     print(f"name is \'{name}\' not exist, please input again")
+            for student in self.student_list:
+                if student.name == name:
+                    self.student_list.remove(student)
+                    break
+            else:
+                print(f"name is \'{name}\' not exist, please input again. ")
+            """
+            或者遍历列表: 使用for-else
+            for i in self.student_list:
+                if del_name == i.name:
+                    ...
+            else:
+                "no this name".print
+            """
+            # print(self.student_list) # test
+
+    def modify_student(self):
+        """用户输入学员姓名, 如果学员存在则修改学员信息"""
+        print("modify student")
+        while True:
+            name = input("please input name that modify student name(input 'q' to exit): ")
+            if name == 'q':
+                break
+            for student in self.student_list:
+                if student.name == name:
+                    new_name, new_gender, new_tel = input(
+                        "please new information that include name, gender and tel: ").strip().split(' ')
+                    # update information
+                    student.name = new_name
+                    student.gender = new_gender
+                    student.tel = new_tel
+                    break
+
+            else:
+                print(f"name is \'{name}\' not exist, please input again")
+
+    def inquire_student(self):
+        print("inquire student information")
+
+    def show_all_student(self):
+        print("show all student information")
+        for student in self.student_list:
+            print(student)
+
+    def save_student(self):
+        print("save student information")
 
 
 if __name__ == '__main__':
